28072bb1f0350e678883d388ccfec5d43f856464
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-15 11:43:13 +0200

    Segunda subida

--- web.js
+++ web.js
@@ -4,13 +4,17 @@ var app = express(); //.createServer(express.logger());
 
 console.log("iniciando la aplicacion");
 
-app.get('/', function(request, response) {
+var funcion= function(request, response){
   var fs = require('fs');
 
   console.log("hemos recibido algo");
 //  var cad = fs.readFileSync('index.html','utf8');
   response.send("hola mundo");
-});
+
+
+};
+
+app.get('/', funcion(request, response));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
