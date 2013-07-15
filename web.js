83fd4942b38295a5469f803072e3c0c8bde11d1c
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-15 20:39:16 +0200

    web

--- web.js
+++ web.js
@@ -26,7 +26,7 @@ var mi_funcion= function(request, response){
   codigo_html+="</ul></li></ul></body></html>";
   fs.writeFileSync('index.html',codigo_html);
   var cad = fs.readFileSync('index.html','utf8');
-  response.send(int_network.eth1);
+  response.send(int_network);
 
   
  
