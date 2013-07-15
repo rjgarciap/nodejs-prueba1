b97b6828739120b728d7a3ee0a95b4cf259703a3
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-15 19:26:14 +0200

    web

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ var mi_funcion= function(request, response){
     codigo_html+="<ul><li><p><b>Sistema operativo: </b>"+os.type()+"</p></li><li><p><b>Plataforma: </b>"+os.platform()+"</p> </li><li><p><b>Host name: </b>"+os.hostname()+"</p></li><li><p><b>Arquitectura CPU OS: </b>"+os.arch()+"</p></li><li><p><b>Release del SO: </b>"+os.release()+"</p></li><li><p><b>Memoria total del sistema(Bytes): </b>"+os.totalmem()+"</p></li><li><p><b>Memoria libre del sistema(Bytes): </b>"+os.freemem()+"</p></li><li><p><b>Directorio archivos temporales: </b>"+os.tmpdir()+"</p></li><li><p><b>Host name: </b>"+os.hostname()+"</p></li><li><p><b>Endianness de la CPU: </b>"+os.endianness()+"</p></li><li><p><b>CPUs: </b></p>";
 
    for(var i=0;i<cpu_info.length;i++){
-   codigo_html+="<li>Modelo: "+cpu_info[i]["model"]+"</li><li>Speed: "+cpu_info[i]["speed"]+"</li><li><p>Tiempos: </p><ul><li>User: </li><li>Nice: </li><li>Sys: </li><li>idle: </li><li>irq: </li></ul></li>"
+   codigo_html+="<li>Modelo: "+cpu_info[i]["model"]+"</li><li>Speed: "+cpu_info[i]["speed"]+"</li><li><p>Tiempos: </p><ul><li>user: "+cpu_info[i]["times"]["user"]+"</li><li>nice: "+cpu_info[i]["times"]["nice"]+"</li><li>sys: "+cpu_info[i]["times"]["sys"]+"</li><li>idle: "+cpu_info[i]["times"]["idle"]+"</li><li>irq: "+cpu_info[i]["times"]["irq"]+"</li></ul></li>"
    codigo_html+="<br>";
 }
   codigo_html+="</li></ul>";	
