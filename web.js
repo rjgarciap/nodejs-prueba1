38c64e95b1acc032cacf3ff69113de909595cae2
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-15 12:20:36 +0200

    Segunda subida

--- web.js
+++ web.js
@@ -13,10 +13,11 @@ var mi_funcion= function(request, response){
   var os_type=os.type();
   var host_name=os.hostname();		
   console.log("hemos recibido algo");
-//  var cad = fs.readFileSync('index.html','utf8');
+  fs.writeFileSync('index.html',"hola");	
+  var cad = fs.readFileSync('index.html','utf8');
   var primero=cpu_info[0]["model"];
   var resultados=["Sistema operativo:",os_type,"Nombre del host:",host_name,"Informacion CPU:",cpu_info,"Interfaces de red:",int_network];
-  response.send(primero);
+  response.send(cad);
  
 };
 
