19d3d78fccdbecdded7f5db10544d027b9194ef7
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 00:01:50 +0200

    segunda prueba json

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var int_network2;
 
 
 //var codigo_html="<html><head></head><body>"; 
-var antjson="{inicio:iniciofinal:final}";
+var antjson="{inicio:inicio,final:final}";
 var json="";
 console.log("iniciando la aplicacion");
 
@@ -29,7 +29,7 @@ json+="{'model':"+cpu_info2[i]["model"]+",'speed':"+cpu_info2[i]["speed"]+"'time
 }else{
 json+="{'model':"+cpu_info2[i]["model"]+",'speed':"+cpu_info2[i]["speed"]+"'times':{'user':"+cpu_info2[i]["times"]["user"]+",'nice':"+cpu_info2[i]["times"]["nice"]+",'sys':"+cpu_info2[i]["times"]["sys"]+",'idle':"+cpu_info2[i]["times"]["idle"]+",'irq':"+cpu_info2[i]["times"]["irq"]+"}},";
 }*/
-	antjson=antjson.replace("final:final}",","+json+",final:final}");
+	antjson=antjson.replace("final:final}",+json+",final:final}");
    //codigo_html+="<li><b>Modelo: </b>"+cpu_info2[i]["model"]+"</li><li><b>Speed: </b>"+cpu_info2[i]["speed"]+"</li><li><b>Tiempos: </b><br><ul><li><b>user: </b>"+cpu_info2[i]["times"]["user"]+"</li><li><b>nice: </b>"+cpu_info2[i]["times"]["nice"]+"</li><li><b>sys: </b>"+cpu_info2[i]["times"]["sys"]+"</li><li><b>idle: </b>"+cpu_info2[i]["times"]["idle"]+"</li><li><b>irq: </b>"+cpu_info2[i]["times"]["irq"]+"</li></ul></li><br>";
 //}
   //codigo_html+="</li><li><p><b>Network Interface: </b></p><ul>";
