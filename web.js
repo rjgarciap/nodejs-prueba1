b914bfdfb461426062e0a29c730f9616bed5069f
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 19:17:38 +0200

    segunda prueba json

--- web.js
+++ web.js
@@ -101,14 +101,15 @@ var funcionJSON= function(request, response){
   var parsed=JSON.parse(data1);
   
   response.set('Content-Type', 'application/json');
-  
+  /*
   if(request.query.q==='medidas'){
   response.send(parsed[request.query.q]);
   }else{
   response.send(data1);
   } 
   
-
+*/
+response.send(parsed['medidas'][0]);
   //response.attachment('informacion.json');
  
 
