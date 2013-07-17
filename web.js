2f63e836c6c735540d3b98000350e7f9c9a5a726
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 15:50:45 +0200

    con hora

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 
 var os = require('os');
 
+//Cuidado con la hora en el json si no se pone como string(\"hora\"), da error porque piensa que este separado.
 
 function hora(){
 	var fecha = new Date();
@@ -23,35 +24,35 @@ function hora(){
 	var freemem = os.freemem();
 	var cpus = os.cpus();
 	var cpustring = "<cputimes>";
-//XML
+//Generando XML
 
-var cpuString2 = "<cputimes>";
+	var cpuString2 = "<cputimes>";
 	
 	for(var i=0; i<cpus.length;i++){
 	
-	cpuString2 += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
+		cpuString2 += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
   
-}
-cpuString2 += "</cputimes>";
-var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas><medida><date>"+hora()+"</date><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString2+"</medida></medidas>";
-//JSON
-var antjson="{\"medidas\":[{\"date\":"+hora()+",\"freememory\":"+freemem+",\"totalMemory\":"+totalmem+",\"uptime\":"+uptime+",\"cputimes\":{";
+	}
+	cpuString2 += "</cputimes>";
+	var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas><medida><date>"+hora()+"</date><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString2+"</medida></medidas>";
+//Generando JSON
+	var antjson="{\"medidas\":[{\"date\":"+hora()+",\"freememory\":"+freemem+",\"totalMemory\":"+totalmem+",\"uptime\":"+uptime+",\"cputimes\":{";
 
-for(var i=0;i<cpus.length;i++){
-if(i===cpus.length-1){
-antjson+="\"user\":"+cpus[i]["times"]["user"]+",\"nice\":"+cpus[i]["times"]["nice"]+",\"sys\":"+cpus[i]["times"]["sys"]+",\"idle\":"+cpus[i]["times"]["idle"]+",\"irq\":"+cpus[i]["times"]["irq"];
-}else{   
-antjson+="\"user\":"+cpus[i]["times"]["user"]+",\"nice\":"+cpus[i]["times"]["nice"]+",\"sys\":"+cpus[i]["times"]["sys"]+",\"idle\":"+cpus[i]["times"]["idle"]+",\"irq\":"+cpus[i]["times"]["irq"]+",";
-}
-};
-antjson+="}}]}"
-var json="";
-console.log("iniciando la aplicacion");
+	for(var i=0;i<cpus.length;i++){
+		if(i===cpus.length-1){
+			antjson+="\"user\":"+cpus[i]["times"]["user"]+",\"nice\":"+cpus[i]["times"]["nice"]+",\"sys\":"+cpus[i]["times"]["sys"]+",\"idle\":"+cpus[i]["times"]["idle"]+",\"irq\":"+cpus[i]["times"]["irq"];
+		}else{   
+			antjson+="\"user\":"+cpus[i]["times"]["user"]+",\"nice\":"+cpus[i]["times"]["nice"]+",\"sys\":"+cpus[i]["times"]["sys"]+",\"idle\":"+cpus[i]["times"]["idle"]+",\"irq\":"+cpus[i]["times"]["irq"]+",";
+		}
+	};
+	antjson+="}}]}"
+	var json="";
+	console.log("iniciando la aplicacion");
 
 
 //repeticion
-var int=setInterval(function(){json_xml_var()},3000);
-var fecha2 = new Date();
+	var int=setInterval(function(){json_xml_var()},3000);
+	var fecha2 = new Date();
 function json_xml_var(){
         var uptime2 = os.uptime();
 	var totalmem2 = os.totalmem();
@@ -63,13 +64,13 @@ function json_xml_var(){
    json+=",{\"date\":"+hora()+",\"freememory\":"+freemem2+",\"totalmemory\":"+totalmem+",\"uptime\":"+uptime2+",\"cputimes\":{";
  
    for(var i=0;i<cpus2.length;i++){
-if(i===cpus2.length-1){   
-json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice"]+",\"sys\":"+cpus2[i]["times"]["sys"]+",\"idle\":"+cpus2[i]["times"]["idle"]+",\"irq\":"+cpus2[i]["times"]["irq"]+"}}";
-}else{   
-json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice"]+",\"sys\":"+cpus2[i]["times"]["sys"]+",\"idle\":"+cpus2[i]["times"]["idle"]+",\"irq\":"+cpus2[i]["times"]["irq"]+",";
-}
+	if(i===cpus2.length-1){   
+		json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice"]+",\"sys\":"+cpus2[i]["times"]["sys"]+",\"idle\":"+cpus2[i]["times"]["idle"]+",\"irq\":"+cpus2[i]["times"]["irq"]+"}}";
+	}else{   
+		json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice"]+",\"sys\":"+cpus2[i]["times"]["sys"]+",\"idle\":"+cpus2[i]["times"]["idle"]+",\"irq\":"+cpus2[i]["times"]["irq"]+",";
+	}
 
-}
+   }
         var newjson=antjson.replace("]}",json+"]}");	
 	//antjson=antjson.replace("]}",json+"]}");
 
@@ -79,9 +80,9 @@ json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice
 	
 	for(var i=0; i<cpus2.length;i++){
 	
-	cpustring += "<user>"+cpus2[i]["times"]["user"]+"</user><nice>"+cpus2[i]["times"]["nice"]+"</nice><sys>"+cpus2[i]["times"]["sys"]+"</sys><idle>"+cpus2[i]["times"]["idle"]+"</idle><irq>"+cpus2[i]["times"]["irq"]+"</irq>";   
+		cpustring += "<user>"+cpus2[i]["times"]["user"]+"</user><nice>"+cpus2[i]["times"]["nice"]+"</nice><sys>"+cpus2[i]["times"]["sys"]+"</sys><idle>"+cpus2[i]["times"]["idle"]+"</idle><irq>"+cpus2[i]["times"]["irq"]+"</irq>";   
   
-}
+	}
 	cpustring += "</cputimes>";	
 
 	var stringNew = string.replace("</medidas>","<medida><date>"+hora()+"</date><uptime>"+uptime2+"</uptime><totalmem>"+totalmem2+"</totalmem><freemem>"+freemem2+"</freemem>"+cpustring+"</medida></medidas>");
@@ -91,7 +92,7 @@ json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice
         fs.writeFileSync('informacion.json',newjson);
 };
 
-var mi_funcion= function(request, response){
+var funcionJSON= function(request, response){
   var fs = require('fs');
   
   console.log("hemos recibido algo");
@@ -108,7 +109,7 @@ var mi_funcion= function(request, response){
  
 };
 
-var mi_funcion2 = function(request, response) {
+var funcionXML = function(request, response) {
   
 
   console.log("hemos recibido algo");
@@ -123,8 +124,8 @@ var mi_funcion2 = function(request, response) {
   
 };
 
-app.get('/xml', mi_funcion2);
-app.get('/json', mi_funcion);
+app.get('/xml', funcionXML);
+app.get('/json', funcionJSON);
 
 
 var port = process.env.PORT || 5050;
