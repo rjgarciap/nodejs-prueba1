dfdd118619f1d27bf3e57f615aabdfd9665b1c21
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 10:57:15 +0200

    segunda prueba json

--- web.js
+++ web.js
@@ -20,6 +20,8 @@ antjson+="\"user\":"+cpu_info2[i]["times"]["user"]+",\"nice\":"+cpu_info2[i]["ti
 antjson+="}}]}"
 var json="";
 console.log("iniciando la aplicacion");
+//XML
+var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas></medidas>"
 
 var int=setInterval(function(){jsonvar()},3000);
 function jsonvar(){
@@ -33,6 +35,28 @@ json+="\"user\":"+cpu_info2[i]["times"]["user"]+",\"nice\":"+cpu_info2[i]["times
 json+="\"user\":"+cpu_info2[i]["times"]["user"]+",\"nice\":"+cpu_info2[i]["times"]["nice"]+",\"sys\":"+cpu_info2[i]["times"]["sys"]+",\"idle\":"+cpu_info2[i]["times"]["idle"]+",\"irq\":"+cpu_info2[i]["times"]["irq"]+",";
 }
 
+
+
+//XML
+
+
+var uptime = os.uptime();
+	var totalmem = os.totalmem();
+	var freemem = os.freemem();
+	var cpus = os.cpus();
+	var cpuString = "<cputimes>";
+	
+	for(var i=0; i<cpus.length;i++){
+	
+	cpuString += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
+  
+}
+	cpuString += "</cputimes>";	
+
+	var stringNew = string.replace("</medidas>","<medida><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString+"</medida></medidas>");
+	
+	string=stringNew;
+	fs.writeFileSync('medidas.xml',stringNew);
 };
 	antjson=antjson.replace("]}",json+"]}");
 
@@ -55,29 +79,7 @@ var mi_funcion= function(request, response){
 
 //Para el xml
 
-var int2  =setInterval(function(){xmlDinamico()},3000);
 
-var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas></medidas>"
-
-var xmlDinamico = function(){
-	var uptime = os.uptime();
-	var totalmem = os.totalmem();
-	var freemem = os.freemem();
-	var cpus = os.cpus();
-	var cpuString = "<cputimes>";
-	
-	for(var i=0; i<cpus.length;i++){
-	
-	cpuString += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
-  
-}
-	cpuString += "</cputimes>";	
-
-	var stringNew = string.replace("</medidas>","<medida><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString+"</medida></medidas>");
-	
-	string=stringNew;
-	fs.writeFileSync('medidas.xml',stringNew);
-}      
 
 
 
