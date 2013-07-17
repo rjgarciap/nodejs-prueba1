6b99a2c769d34a626ec42b55fc29f58c5c9a26cc
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 15:40:09 +0200

    con hora

--- web.js
+++ web.js
@@ -11,12 +11,12 @@ function hora(){
 	var hora = fecha.getHours()
 	var minuto = fecha.getMinutes()
 	var segundo = fecha.getSeconds()
-	if (hora < 10) {hora = "0" + hora}
-	if (minuto < 10) {minuto = "0" + minuto}
-	if (segundo < 10) {segundo = "0" + segundo}
-	var horita = hora + ":" + minuto + ":" + segundo
+	if (hora < 10) {hora ="0"+ hora}
+	if (minuto < 10) {minuto ="0"+ minuto}
+	if (segundo < 10) {segundo ="0"+ segundo}
+	var horita = hora +":"+ minuto +":"+ segundo
 	
-        return horita;
+        return 0;
 }
 	var uptime = os.uptime();
 	var totalmem = os.totalmem();
