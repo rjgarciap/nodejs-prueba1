e6835950d650a0fa09f666542b282353969a6284
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 12:27:58 +0200

    prueba4 xml y json

--- web.js
+++ web.js
@@ -83,7 +83,7 @@ json+="\"user\":"+cpus2[i]["times"]["user"]+",\"nice\":"+cpus2[i]["times"]["nice
 }
 	cpustring += "</cputimes>";	
 
-	var stringNew = string.replace("</medidas>","<medida><date>"+hora()+"</date><uptime>"+uptime2+"</uptime><totalmem>"+totalmem2+"</totalmem><freemem2>"+freemem2+"</freemem>"+cpustring+"</medida></medidas>");
+	var stringNew = string.replace("</medidas>","<medida><date>"+hora()+"</date><uptime>"+uptime2+"</uptime><totalmem>"+totalmem2+"</totalmem><freemem>"+freemem2+"</freemem>"+cpustring+"</medida></medidas>");
 	
 	string=stringNew;
 	fs.writeFileSync('medidas.xml',stringNew);
