99835ba85a9df50a466c1babb5a3b2a5c4aa68da
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-17 10:52:54 +0200

    segunda prueba json

--- web.js
+++ web.js
@@ -53,7 +53,55 @@ var mi_funcion= function(request, response){
  
 };
 
+//Para el xml
+
+var int2  =setInterval(function(){xmlDinamico()},3000);
+
+var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas></medidas>"
+
+var xmlDinamico = function(){
+	var uptime = os.uptime();
+	var totalmem = os.totalmem();
+	var freemem = os.freemem();
+	var cpus = os.cpus();
+	var cpuString = "<cputimes>";
+	
+	for(var i=0; i<cpus.length;i++){
+	
+	cpuString += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
+  
+}
+	cpuString += "</cputimes>";	
+
+	var stringNew = string.replace("</medidas>","<medida><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString+"</medida></medidas>");
+	
+	string=stringNew;
+	fs.writeFileSync('medidas.xml',stringNew);
+}      
+
+
+
+
+var miFuncion2 = function(request, response) {
+  
+
+  console.log("hemos recibido algo");
+
+  //Si quito 2º parámetro (encoding) al entrar en la web me deja descargar el xml perfect y si pongo  utf-8 no sale el texto como xml. Había que usar response.set()!!!!
+
+  fs.readFile('medidas.xml', function (err, data) {
+  if (err) throw err;
+  response.set('Content-Type', 'text/xml');
+  response.send(data); 
+  });
+
+  //response.attachment('medidas.xml');
+  
+};
+
+
 app.get('/json', mi_funcion);
+app.get('/xml', mi_funcion2);
 
 var port = process.env.PORT || 5190;
 app.listen(port, function() {
